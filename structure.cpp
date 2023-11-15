def7e2f7ca9b2eb52b0b0ffe0c71b566b5ce72fb
aditya singh <sadityaa73@gmail.com>
2023-11-15 16:44:52 +0530

    sturucture

--- structure.cpp
+++ structure.cpp
@@ -13,6 +13,16 @@ void display(stu s1){
     cout<<"student_roll"<<":-"<<s1.roll<<endl;
     cout<<"student_marks"<<":-"<<s1.marks<<endl;
     
+};
+int sumOfMarks(stu s1){
+    s1.marks = 20.4; 
+    cout<<"printing marks in function"<<":"<<s1.marks<<endl;
+    return 0;
+};
+void nameOfStu(stu &s1)
+{
+    s1.name ="aashu";
+    cout<<"printing name of student from inside the function\t"<<":"<<s1.name<<endl;
 }
 int main()
 {
@@ -23,5 +33,11 @@ int main()
 
     // passing structure in funciton:
     display(s1);
+    //passing structure in function as value :
+    sumOfMarks(s1);
+    cout<<"after passing structure as value"<<":-"<<s1.marks;
+    //passing structure in funciton as reference:
+    nameOfStu(s1);
+    cout<<"after passing structure as refernce"<<":-"<<s1.name;
     return 0;
 }
