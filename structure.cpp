ce5b0504d9f0e60fd3a84fa6a8b4c0c6c4a11d43
aditya singh <sadityaa73@gmail.com>
2023-11-15 23:20:25 +0530

    structure modified

--- structure.cpp
+++ structure.cpp
@@ -8,22 +8,26 @@ typedef struct student{
     int roll;
     float marks;
 }stu;
+//structure passing into funciton;
 void display(stu s1){
     cout<<"student_name"<<":-"<<s1.name<<endl;
     cout<<"student_roll"<<":-"<<s1.roll<<endl;
     cout<<"student_marks"<<":-"<<s1.marks<<endl;
     
 };
+//pass by value:
 int sumOfMarks(stu s1){
     s1.marks = 20.4; 
     cout<<"printing marks in function"<<":"<<s1.marks<<endl;
     return 0;
 };
+//pass by reference:
 void nameOfStu(stu &s1)
 {
     s1.name ="aashu";
     cout<<"printing name of student from inside the function\t"<<":"<<s1.name<<endl;
-}
+};
+
 int main()
 {
     stu s1;
