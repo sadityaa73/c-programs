bcb1339e5d709392df556ab3601d1478df14397f
aditya singh <sadityaa73@gmail.com>
2023-11-28 21:12:09 +0530

    insertion of array element at start ,end and mid

---
+++ ArrayOperations.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+const int Max_Size = 100;
+
+void display(int arr[],int& size)
+{
+    cout<<"size of array "<<size<<endl;
+    cout<<"[";
+    for(int i=0;i<=size-1;i++)
+    {
+        cout<<arr[i];
+        if(i != size-1)
+        cout<<",";
+    }
+    cout<<"]";
+    cout<<endl;
+}
+
+int insertAtBegain(int arr[],int& size,int element)
+{
+    for(int i=size;i>0;i--)
+    {
+        arr[i] = arr[i-1]; 
+    }
+    arr[0] = element;
+    size++;
+    display(arr,size);
+    return 0;
+}
+
+int insertAtEnd(int arr[],int& size,int element)
+{
+    arr[size] = element;
+    size++;
+    display(arr,size);
+    return 0;
+}
+
+int insertAtMid(int arr[],int& size,int element)
+{
+    int start=0,end=size-1,mid=(start+end)/2;
+    cout<<"mid "<<mid<<endl;
+    for(int i=size;i>=mid;i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[mid]=element;
+    size++;
+    display(arr,size);
+    return 0;
+}
+int main()
+{
+    int arr[Max_Size]={2,6,8,4};
+    int size = 4;
+    
+    display(arr,size);
+    insertAtBegain(arr,size,1);
+    insertAtBegain(arr,size,10);
+    insertAtEnd(arr,size,22);
+    insertAtEnd(arr,size,34);
+    insertAtMid(arr,size,55);
+    return 0;
+}
